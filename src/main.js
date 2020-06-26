2430382048207665178577f72f3d11a0cb0a704a
MFRamon <ramon.manfig@gmail.com>
2020-06-26 11:29:27 -0500

    Buefy Table

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 import router from "./router";
+import { Table, Input } from "buefy";
+import "buefy/dist/buefy.css";
 
+Vue.use(Table);
+Vue.use(Input);
+Vue.use(Buefy);
 Vue.config.productionTip = false;
 
 new Vue({
